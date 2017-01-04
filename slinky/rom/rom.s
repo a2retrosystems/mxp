948341b9d2488b0b8726816144e1681c137fdfa8
Oliver Schmidt <ol.sc@web.de>
2017-01-04 23:08:53 +0100

    Added Slinky ROM source.
    This source reproduces the original Slinky ROM. It was extracted from an Apple ROM listing and modified to be compatible with the cc65 tool chain.

---
+++ slinky/rom/rom.s
@@ -0,0 +1,100 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; Driver for the ramdisk
+; for the Apple II computer
+;
+; by Rich Williams
+; Version 1.0 September 1985
+;
+; Copyright Apple Computer, Inc. 1985
+; All rights reserved
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+.macpack  generic
+.macpack  apple2
+.feature  labels_without_colons
+.feature  loose_string_term
+.define   equ =
+.define   asc .byte
+.define   dfb .byte
+.define   dw  .word
+
+.include  'equates.inc'
+
+.segment  'msg'
+          asc 'This wonderful, wonderful rom'
+          asc 'is brought to you by Richard Williams'
+          asc 'and should only be used for'
+          asc 'the benefit of mankind'
+
+.macro    bootromdefs
+          dfb pcrevnum        ; Smartport revision number
+          dfb $01             ; Mark ram card
+          dw 0                ; Number of blocks = 0 for status call
+          dfb $4F
+          dfb <entry
+.endmacro
+
+.scope    boot1
+slot      equ 1
+.segment  'boot1'
+.include  'bootrom.inc'
+.segment  'defs1'
+          bootromdefs
+.endscope
+
+.scope    boot2
+slot      equ 2
+.segment  'boot2'
+.include  'bootrom.inc'
+.segment  'defs2'
+          bootromdefs
+.endscope
+
+.scope    boot3
+slot      equ 3
+.segment  'boot3'
+.include  'bootrom.inc'
+.segment  'defs3'
+          bootromdefs
+.endscope
+
+.scope    boot4
+slot      equ 4
+.segment  'boot4'
+.include  'bootrom.inc'
+.segment  'defs4'
+          bootromdefs
+.endscope
+
+.scope    boot5
+slot      equ 5
+.segment  'boot5'
+.include  'bootrom.inc'
+.segment  'defs5'
+          bootromdefs
+.endscope
+
+.scope    boot6
+slot      equ 6
+.segment  'boot6'
+.include  'bootrom.inc'
+.segment  'defs6'
+          bootromdefs
+.endscope
+
+.scope    boot7
+slot      equ 7
+.segment  'boot7'
+.include  'bootrom.inc'
+.segment  'defs7'
+          bootromdefs
+.endscope
+
+.segment  'exec'
+.include  'execute.inc'
+.include  'makecat.inc'
+
+.segment  'diags'
+.include  'diags.inc'
